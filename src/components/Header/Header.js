994235768c2160102e4464507c6d2cefa7129f9d
seanh101 <seanharrington4189@gmail.com>
2023-10-20 14:39:18 -0400

    changed styling to green and gold, changed fonts and added truck image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import './Header.css';
 function Header() {
   return (
     <div className='header'>
-        <h1>Mallard Oil</h1>
-        <p className='business-info'>Keeping South Shore warm for over 40 years</p>
+        <div className='company'><h1>MALLARD OIL</h1></div>
+        <p className='business-info'>Keeping South Shore MA warm for over 40 years</p>
       <p className='phone-number'>781-740-4767</p>
-
+      <h4 className='motto'>"We don't fly south for the winter!" </h4>
     </div>
   );
 }
